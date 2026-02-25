895b64df6db4d5d13df2b9e8a9a9d4b514d0a2b3
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-02-25 20:17:26 +0900

    Change import from types to models in GPU file

--- residue/residue_structures_gpu.py
+++ residue/residue_structures_gpu.py
@@ -22,7 +22,7 @@ except ImportError:
     cp = None
 
 # Local imports
-from ..types import ArrayType, NDArray
+from ..models import ArrayType, NDArray
 from ..core import GPUBackend, GPUMemoryManager, handle_gpu_errors
 
 # residue_com_kernelのインポート（エラー時はNone）
