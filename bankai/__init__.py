a9a91e793d3b77bc016b630f187ba4f5acd0954d
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-02-27 14:07:27 +0900

    Bump version to 1.1.3

--- bankai/__init__.py
+++ bankai/__init__.py
@@ -25,7 +25,7 @@ from typing import Any
 # Version
 # ===============================
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __author__ = "Masamichi Iizumi"
 
 # ===============================
