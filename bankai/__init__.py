f61aa912cc680f710ed7366d485dd297ab97287d
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-05-17 10:25:52 +0900

    Update __init__.py

--- bankai/__init__.py
+++ bankai/__init__.py
@@ -25,7 +25,7 @@ from typing import Any
 # Version
 # ===============================
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "Masamichi Iizumi"
 
 # ===============================
